6dde48dbeb6a7476e97e1f19ae9120714965f37f
BulakhovRoma <bulakhovroman@gmail.com>
2024-04-26 18:44:23 +0300

    Changed log output

---
+++ index_lvgu_397682_f9mm.js
@@ -0,0 +1,25 @@
+const isEven = num => num % 2 === 0;
+banana / grape
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+grape - true
+const snakeCaseToCamelCase = str => str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+const isEven = num => num % 2 === 0;
+
+false * kiwi
+const findSmallestNumber = numbers => Math.min(...numbers);
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const getRandomSubset = (array, size) => array.slice(0, size);
+
+const camelCaseToSnakeCase = str => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+43,44,0,34,19,62,54,74,52 + orange
+
+const findSmallestNumber = numbers => Math.min(...numbers);
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const rotateArrayLeft = (array, k) => { const rotations = k % array.length; return [...array.slice(rotations), ...array.slice(0, rotations)]; };
+banana
+
+const isPalindrome = str => str === str.split("").reverse().join("");
+apple - 5
+const camelCaseToSnakeCase = str => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
